cdd3dc30263567e5e5b3c3d0209117db8229b3b8
Rajat Avoma <79824085+rajatkulkarni@users.noreply.github.com>
2022-06-28 14:48:17 +0530

    qwefsdrgtr (#10)
    * qwefsdrgtr
    
    * asdasd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Something
+          Something else
         </a>
       </header>
     </div>
